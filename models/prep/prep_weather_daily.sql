24440dadac577c2dbd2161271429effa4f14e2d2
Munzer <munzeralawad82@gmail.com>
2026-02-25 21:26:18 +0100

    Add prep_weather_daily model

--- models/prep/prep_weather_daily.sql
+++ models/prep/prep_weather_daily.sql
@@ -3,22 +3,22 @@ WITH daily_data AS (
     FROM {{ ref('staging_weather_daily') }}
 ),
 add_features AS (
-    SELECT *
-        , DATE_PART('day', date) AS date_day
-        , DATE_PART('month', date) AS date_month
-        , DATE_PART('year', date) AS date_year
-        , DATE_PART('week', date) AS cw
-        , TO_CHAR(date, 'FMmonth') AS month_name
-        , TO_CHAR(date, 'FMday') AS weekday
+    SELECT *,
+        DATE_PART('day', date) AS date_day,
+        DATE_PART('month', date) AS date_month,
+        DATE_PART('year', date) AS date_year,
+        DATE_PART('week', date) AS cw,
+        TO_CHAR(date, 'FMmonth') AS month_name,
+        TO_CHAR(date, 'FMday') AS weekday
     FROM daily_data
 ),
 add_more_features AS (
-    SELECT *
-        , CASE 
-            WHEN month_name IN ('december','january','february') THEN 'winter'
-            WHEN month_name IN ('march','april','may') THEN 'spring'
-            WHEN month_name IN ('june','july','august') THEN 'summer'
-            WHEN month_name IN ('september','october','november') THEN 'autumn'
+    SELECT *,
+        CASE 
+            WHEN month_name in ('december','january','february') THEN 'winter'
+            WHEN month_name in ('march','april','may') THEN 'spring'
+            WHEN month_name in ('june','july','august') THEN 'summer'
+            WHEN month_name in ('september','october','november') THEN 'autumn'
         END AS season
     FROM add_features
 )
