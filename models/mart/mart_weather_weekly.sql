7783ac80ebcbf882c809cd8964e7d685f7bd75dd
Munzer <munzeralawad82@gmail.com>
2026-02-26 13:53:32 +0100

    Fix mart_weather_weekly with correct column names

--- models/mart/mart_weather_weekly.sql
+++ models/mart/mart_weather_weekly.sql
@@ -1,10 +1,10 @@
 SELECT
-    date_trunc('week', w.date) AS week_start,
-    AVG(w.daily_min_temp) AS avg_min_temp,
-    AVG(w.daily_max_temp) AS avg_max_temp,
-    SUM(w.daily_precipitation) AS total_precipitation,
-    SUM(w.daily_snowfall) AS total_snowfall,
-    AVG(w.daily_avg_wind_speed) AS avg_wind_speed
-FROM {{ ref('prep_weather_daily') }} w
-GROUP BY 1
-ORDER BY 1
+    DATE_TRUNC('week', date) AS week_start,
+    AVG(min_temp_c) AS avg_min_temp_c,
+    AVG(max_temp_c) AS avg_max_temp_c,
+    SUM(precipitation_mm) AS total_precipitation_mm,
+    SUM(max_snow_mm) AS total_snow_mm,
+    AVG(avg_wind_speed_kmh) AS avg_wind_speed_kmh
+FROM {{ ref('prep_weather_daily') }}
+GROUP BY DATE_TRUNC('week', date)
+ORDER BY week_start
