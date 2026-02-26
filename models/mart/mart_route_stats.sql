d0ceabc8be6060a28678485d86ebb8893ccd18ef
Munzer <munzeralawad82@gmail.com>
2026-02-26 14:07:52 +0100

    Add mart_route_stats.sql with corrected arr_delay column

--- models/mart/mart_route_stats.sql
+++ models/mart/mart_route_stats.sql
@@ -5,10 +5,10 @@ SELECT
     COUNT(DISTINCT f.tail_number) AS unique_planes,
     COUNT(DISTINCT f.airline) AS unique_airlines,
     AVG(f.actual_elapsed_time) AS avg_elapsed_time,
-    AVG(f.arrival_delay) AS avg_arrival_delay,
-    MAX(f.arrival_delay) AS max_delay,
-    MIN(f.arrival_delay) AS min_delay,
+    AVG(f.arr_delay) AS avg_arr_delay,
+    MAX(f.arr_delay) AS max_arr_delay,
+    MIN(f.arr_delay) AS min_arr_delay,
     SUM(CASE WHEN f.cancelled = 1 THEN 1 ELSE 0 END) AS total_cancelled,
     SUM(CASE WHEN f.diverted = 1 THEN 1 ELSE 0 END) AS total_diverted
-FROM {{ ref('prep_flights') }} f
+FROM s_munzeralawad.prep_flights f
 GROUP BY f.origin, f.dest
